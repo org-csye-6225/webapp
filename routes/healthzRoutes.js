542c3a8e733001e047664b7f097229be5f75786c
Abhinav-NEU <abhinavp.klg@gmail.com>
2024-02-07 20:50:52 -0500

    fix: added routes checks as middleware

--- routes/healthzRoutes.js
+++ routes/healthzRoutes.js
@@ -3,35 +3,7 @@ const db = require('../init/index');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-const commonHeaders = {
-  'Cache-Control': 'no-cache, no-store, must-revalidate',
-};
-
-const rejectPayload = (req, res, next) => {
-  if (Object.keys(req.body).length > 0 || Object.keys(req.query).length > 0) {
-    res.status(400).header(commonHeaders).send();
-  } else {
-    next();
-  }
-};
-
-const checkDatabaseConnection = async () => {
-  try {
-    await db.sequelize.authenticate();
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-
-const checkDBConnection = async (req, res, next) => {
-  try {
-    await checkDatabaseConnection();
-    next();
-  } catch (error) {
-    res.status(503).header(commonHeaders).send();
-  }
-};
+const { commonHeaders, rejectPayload, checkDBConnection } = require('../middleware/routes');
 
 router.get('/', rejectPayload, checkDBConnection, (req, res) => {
   res.status(200).header(commonHeaders).send();
