9faac6ad0d7290099fa4337a7ce294411aaabe11
Abhinav-NEU <abhinavp.klg@gmail.com>
2024-02-07 20:09:23 -0500

    added route auth for basic auth

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const basicAuth = require('basic-auth');
+const compare = require('tsscmp');
+const auth = require('../config/AuthConfig');
+
+const authenticateBasic = (req, res, next) => {
+  const user = basicAuth(req);
+
+  if (!user ||
+    !compare(user.name, auth.AUTH_USER) ||
+    !compare(user.pass, auth.AUTH_PSWD)) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.sendStatus(401);
+  }
+
+  req.authenticatedUser = {
+    email: user.names,
+  };
+
+  next();
+};
+
+module.exports = authenticateBasic;
