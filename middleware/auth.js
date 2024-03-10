08335206b419169bbdfaf50d799c8feba244ea85
Abhinav-NEU <abhinavp.klg@gmail.com>
2024-03-10 17:10:26 -0400

    test

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,11 +14,18 @@ const authenticateBasic = (req, res, next) => {
   const isUserNameValid = compare(user.name, auth.AUTH_USER);
   const isPasswordValid = compare(user.pass, auth.AUTH_PSWD);
  
+  console.log("User", user);
 
   console.log('Username valid:', isUserNameValid);
-  console.log('Password valid:', isPasswordValid);  
+  console.log('Password valid:', isPasswordValid);
+  console.log("user.name", auth.AUTH_USER);
   console.log('user.pass:', user.pass);
   console.log('auth.AUTH_PSWD:', auth.AUTH_PSWD);
+  console.log('sqlUSER:', process.env.SQL_USER);
+  console.log('sqlPSWD:', process.env.SQL_PSWD);
+
+ 
+
 
   //remove above code
 
