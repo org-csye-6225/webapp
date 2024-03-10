ddffdb7cc3fc9422f0795f53f3e9462d96f6a895
Abhinav-NEU <abhinavp.klg@gmail.com>
2024-03-10 17:10:26 -0400

    test

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,32 +3,7 @@ const compare = require('tsscmp');
 const auth = require('../config/AuthConfig');
 
 const authenticateBasic = (req, res, next) => {
-
   const user = basicAuth(req);
-  // remove below code
-  if (!user) {
-    console.log('No user provided');
-    // ... handle authentication failure
-  }
-
-  const isUserNameValid = compare(user.name, auth.AUTH_USER);
-  const isPasswordValid = compare(user.pass, auth.AUTH_PSWD);
- 
-  console.log("User", user);
-
-  console.log('Username valid:', isUserNameValid);
-  console.log('Password valid:', isPasswordValid);
-  console.log("user.name", auth.AUTH_USER);
-  console.log('user.pass:', user.pass);
-  console.log('auth.AUTH_PSWD:', auth.AUTH_PSWD);
-  console.log('sqlUSER:', process.env.SQL_USER);
-  console.log('sqlPSWD:', process.env.SQL_PSWD);
-
- 
-
-
-  //remove above code
-
 
   if (!user ||
     !compare(user.name, auth.AUTH_USER) ||
