f8febb0b56f7342ccdc3001b0ed89a72d6dd5df9
Abhinav-NEU <abhinavp.klg@gmail.com>
2024-03-20 18:20:40 -0400

    added more logging

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 const basicAuth = require('basic-auth');
 const compare = require('tsscmp');
 const auth = require('../config/AuthConfig');
+const logger = require('../logging/logger');
 
 const authenticateBasic = (req, res, next) => {
   const user = basicAuth(req);
@@ -9,13 +10,14 @@ const authenticateBasic = (req, res, next) => {
     !compare(user.name, auth.AUTH_USER) ||
     !compare(user.pass, auth.AUTH_PSWD)) {
     res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    logger.error('Authentication failed');
     return res.sendStatus(401);
   }
 
   req.authenticatedUser = {
     email: user.name,
   };
-
+  logger.info(`User ${user.name} authenticated successfully`); 
   next();
 };
 
