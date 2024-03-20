2e36dda9ab4f5deff760c9321e80a6dc2af2f86a
Abhinav-NEU <abhinavp.klg@gmail.com>
2024-03-20 19:04:23 -0400

    fix for githubactions

--- logging/logger.js
+++ logging/logger.js
@@ -1,78 +1,83 @@
-const { createLogger, transports, format } = require('winston');
-const { LoggingWinston } = require('@google-cloud/logging-winston');
+// logging/logger.js
 
-const loggingWinston = new LoggingWinston();
+if (!process.env.GITHUB_ACTIONS) {
+  const { createLogger, transports, format } = require('winston');
+  const { LoggingWinston } = require('@google-cloud/logging-winston');
 
-const fs = require('fs');
-const path = require('path');
+  const loggingWinston = new LoggingWinston();
 
-const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+  const fs = require('fs');
+  const path = require('path');
 
-let logDirectory;
-const pathVal = '../../../../../var/log/webapp/'
-if (isGitHubActions) {
-  return true;
-} else {
-  logDirectory = path.join(__dirname, pathVal);
-}
+  const logDirectory = path.join(__dirname, '../../../../../var/log/webapp/');
 
-if (!fs.existsSync(logDirectory)) {
-  fs.mkdirSync(logDirectory);
-}
-
-const customFormat = format.printf(({ level, message, timestamp, stack }) => {
-  let severityLevel;
-  switch (level) {
-    case 'debug':
-      severityLevel = 'DEBUG';
-      break;
-    case 'info':
-      severityLevel = 'INFO';
-      break;
-    case 'warn':
-      severityLevel = 'WARNING';
-      break;
-    case 'error':
-      severityLevel = 'ERROR';
-      break;
-    default:
-      severityLevel = level.toUpperCase();
+  // Create log directory if it doesn't exist
+  if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory);
   }
-  const logObject = {
-    timestamp,
-    severity: severityLevel,
-    message,
-    ...(stack ? { stack } : {})
-  };
-  if (level === 'error' && stack) {
-    const errorObject = stack.split('\n')[0];
-    const fileLineRegex = /\((.+?):(\d+):(\d+)\)/;
-    const match = errorObject.match(fileLineRegex);
 
-    if (match) {
-      logObject.file = match[1];
-      logObject.line = match[2];
-      logObject.col = match[3];
+  const customFormat = format.printf(({ level, message, timestamp, stack }) => {
+    let severityLevel;
+    switch (level) {
+      case 'debug':
+        severityLevel = 'DEBUG';
+        break;
+      case 'info':
+        severityLevel = 'INFO';
+        break;
+      case 'warn':
+        severityLevel = 'WARNING';
+        break;
+      case 'error':
+        severityLevel = 'ERROR';
+        break;
+      default:
+        severityLevel = level.toUpperCase();
     }
-  }
-  return JSON.stringify(logObject);
-});
+    const logObject = {
+      timestamp,
+      severity: severityLevel,
+      message,
+      ...(stack ? { stack } : {})
+    };
+    if (level === 'error' && stack) {
+      const errorObject = stack.split('\n')[0];
+      const fileLineRegex = /\((.+?):(\d+):(\d+)\)/;
+      const match = errorObject.match(fileLineRegex);
 
-const logger = createLogger({
-  level: 'debug',
-  format: format.combine(
-    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json(),
-    customFormat
-  ),
+      if (match) {
+        logObject.file = match[1];
+        logObject.line = match[2];
+        logObject.col = match[3];
+      }
+    }
+    return JSON.stringify(logObject);
+  });
 
-  transports: [
-    new transports.Console(),
-    new transports.File({ filename: path.join(logDirectory, 'combined.log') }),
-    loggingWinston
-  ]
-});
+  const logger = createLogger({
+    level: 'debug',
+    format: format.combine(
+      format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      format.errors({ stack: true }),
+      format.splat(),
+      format.json(),
+      customFormat
+    ),
 
-module.exports = logger;
+    transports: [
+      new transports.Console(),
+      new transports.File({ filename: path.join(logDirectory, 'combined.log') }),
+      loggingWinston
+    ]
+  });
+
+  module.exports = logger;
+} else {
+  // Export a dummy logger for GitHub Actions
+  module.exports = {
+    debug: () => {},
+    info: () => {},
+    warn: () => {},
+    error: () => {}
+  };
+}
