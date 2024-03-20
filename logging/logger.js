928611a2e9aa4b1319ef4b0afe41af0c74b19850
Abhinav-NEU <abhinavp.klg@gmail.com>
2024-03-20 07:30:39 -0400

    logger severity fix

--- logging/logger.js
+++ logging/logger.js
@@ -13,12 +13,40 @@ if (!fs.existsSync(logDirectory)) {
 }
 
 const customFormat = format.printf(({ level, message, timestamp, stack }) => {
+  let severityLevel;
+  switch (level) {
+    case 'debug':
+      severityLevel = 'DEBUG';
+      break;
+    case 'info':
+      severityLevel = 'INFO';
+      break;
+    case 'warn':
+      severityLevel = 'WARNING';
+      break;
+    case 'error':
+      severityLevel = 'ERROR';
+      break;
+    default:
+      severityLevel = level.toUpperCase();
+  }
   const logObject = {
     timestamp,
-    severity: level,
+    severity: severityLevel,
     message,
     ...(stack ? { stack } : {})
   };
+  if (level === 'error') {
+    const errorObject = stack.split('\n')[0];
+    const fileLineRegex = /\((.+?):(\d+):(\d+)\)/;
+    const match = errorObject.match(fileLineRegex);
+
+    if (match) {
+      logObject.file = match[1];
+      logObject.line = match[2];
+      logObject.col = match[3];
+    }
+  }
   return JSON.stringify(logObject);
 });
 
